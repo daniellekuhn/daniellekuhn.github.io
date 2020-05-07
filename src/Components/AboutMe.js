a8ed06d5c3bc6c633a82ce2177166519d07c0f5b
Danielle Kuhn <danielle.r.kuhn@gmail.com>
2020-05-07 08:19:04 -0500

    add file structure and framework for components

---
+++ src/Components/AboutMe.js
@@ -0,0 +1,38 @@
+import React from 'react';
+// import './App.css';
+
+const AboutMe = () => {
+    return (
+        <div>
+            <div className="container mt-3">
+                <div className="d-flex justify-content-center mt-4">
+                <h3 style={{borderBottom: "2px solid #6666ff"}} href="/AboutMe">About Me</h3>
+            </div>
+            <div className="row mt-3">
+                <div className="col-4">
+                    <div className="embed-responsive embed-responsive-1by1">
+                        <img src="https://i.imgur.com/SGomD7x.jpg" alt="..." className="embed-responsive-item"></img>
+                    </div>
+                    <div>
+                        <p className="mb-0">Danielle Kuhn</p>
+                        <p className="mb-0">danielle.r.kuhn@gmail.com</p>
+                        <p className="mb-0">Github | Linkedin </p>
+                    </div>
+                </div>
+                <div className="col-8">
+                    <p>My name is Danielle Kuhn and I am a full-stack software engineer. I love building seamless, dynamic applications for users!</p>
+                    <p>I entered the tech industry about five years ago when 
+                    I founded a <a style={{color: "black"}} href="https://pingpongpayments.com/">Fintech company</a> because I love solving complex problems, especially through the use of technology. Throughout that journey I realized 
+                    that I am enamored with engineering challenges and what can be accomplished with code. So I decided to follow my passion in software engineering and 
+                    have been creating applications ever since. </p>
+                    <p>Check out my <a style={{color: "black"}} href="https://github.com/daniellekuhn">portfolio</a> for some of my recent applications!</p>
+                </div>
+            </div>
+        </div>
+        </div>
+    )
+}
+
+export default AboutMe;
+
+
---
+++ src/Components/Navbar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+// import './App.css';
+
+const Navbar = () => {
+    return (
+        <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="...">Danielle Kuhn</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li className="nav-item">
+                <a className="nav-link" href="...">About Me</a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link" href="...">Skills</a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link" href="...">Experience</a>
+                </li>
+            </ul>
+            </div>
+        </nav>
+        </div>
+    );
+}
+
+export default Navbar;
+
+
+
+
+
