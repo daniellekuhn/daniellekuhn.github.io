c6f828e4aaaaed50a392c713b406ffffe01ab813
Danielle Kuhn <danielle.r.kuhn@gmail.com>
2020-05-18 14:32:52 -0500

    create main components and link to App

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import ImageBar from './Components/ImageBar'
 import AboutMe from './Components/AboutMe'
 import Skills from './Components/Skills'
 import Experience from './Components/Experience'
-import './App.css';
 
 function App() {
   return (
