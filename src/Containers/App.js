cc153334c1172ddaecaec54a634a824337a29b7a
Danielle Kuhn <danielle.r.kuhn@gmail.com>
2020-05-21 08:18:39 -0500

    add react router and hash routes

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -3,17 +3,21 @@ import Navbar from '../Components/Navbar/Navbar'
 import ImageBar from '../Components/ImageBar/ImageBar'
 import AboutMe from '../Components/AboutMe/AboutMe'
 import Projects from '../Components/Projects/Projects'
-import Experience from '../Components/Experience/Experience'
-import { Route, BrowserRouter } from 'react-router-dom'
+import ContactMe from '../Components/ContactMe/ContactMe'
+// import Experience from '../Components/Experience/Experience'
+import { BrowserRouter } from 'react-router-dom'
+import styles from './App.module.css'
 
 function App() {
   return (
     <BrowserRouter>
+    <div id="home" className={styles['background']}>
         <Navbar/>
-        <Route path="/" exact component={ImageBar}></Route>
-        <Route path="/AboutMe" exact component={AboutMe}></Route>
-        <Route path="/MyProjects" exact component={Projects}></Route>
-        <Route path="/Experience" exact component={Experience}></Route>
+        <ImageBar/>
+        <AboutMe />
+        <Projects />
+        <ContactMe />
+      </div>
     </BrowserRouter>
   );
 }
