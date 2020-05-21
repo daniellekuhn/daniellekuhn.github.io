d1c195d176ffac04bef29dec551b2fcce306bc4f
Danielle Kuhn <danielle.r.kuhn@gmail.com>
2020-05-21 09:38:39 -0500

    Updates

---
+++ precache-manifest.d8951b832051be3f4a5f196a8bb56de3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea508c14078c91ffe122f036bf495d4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "129f450db9e88146ef98",
+    "url": "/portfolio/static/css/main.8f5f44c2.chunk.css"
+  },
+  {
+    "revision": "6be135198ed24db34cff",
+    "url": "/portfolio/static/js/2.2c0c554b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.2c0c554b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "129f450db9e88146ef98",
+    "url": "/portfolio/static/js/main.81f00b8f.chunk.js"
+  },
+  {
+    "revision": "27e15bd3e25e8b99391d",
+    "url": "/portfolio/static/js/runtime-main.0df6e538.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/daniellekuhn/precache-manifest.3ce95da669e8a6b7fb9085c54c3d2000.js"
+  "/portfolio/precache-manifest.d8951b832051be3f4a5f196a8bb56de3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/daniellekuhn/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
