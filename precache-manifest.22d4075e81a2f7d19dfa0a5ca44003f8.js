ad3c8a649c15c56500ca0883d69cd0972ade25e0
Danielle Kuhn <danielle.r.kuhn@gmail.com>
2020-05-21 08:56:08 -0500

    Updates

---
+++ precache-manifest.22d4075e81a2f7d19dfa0a5ca44003f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db3f7b3d50482117c2a683b221c9736e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "507c6fbb87ff6086053d",
+    "url": "/static/css/main.8f5f44c2.chunk.css"
+  },
+  {
+    "revision": "6be135198ed24db34cff",
+    "url": "/static/js/2.2c0c554b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2c0c554b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "507c6fbb87ff6086053d",
+    "url": "/static/js/main.991bee40.chunk.js"
+  },
+  {
+    "revision": "5623ad74d8e72796d082",
+    "url": "/static/js/runtime-main.0c215e12.js"
+  }
+]);
