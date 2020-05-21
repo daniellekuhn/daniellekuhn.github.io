79af53bee246a09167d0279f92f36a8ecc1a1646
Danielle Kuhn <danielle.r.kuhn@gmail.com>
2020-05-21 17:46:54 -0500

    Updates

--- precache-manifest.3517cd00c5713c8ba046f1898aebb859.js
+++ precache-manifest.3517cd00c5713c8ba046f1898aebb859.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "82348d3647e4dadd83345f76911db646",
+    "revision": "f12fa33df97f43690951095a9cd1a41b",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "d1abaa2ec92ed2a3d2a9",
-    "url": "/portfolio/static/css/main.8f5f44c2.chunk.css"
+    "revision": "48cc490cc325cbabab5b",
+    "url": "/portfolio/static/css/main.466ea7cb.chunk.css"
   },
   {
     "revision": "6be135198ed24db34cff",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.2c0c554b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d1abaa2ec92ed2a3d2a9",
-    "url": "/portfolio/static/js/main.72995ddc.chunk.js"
+    "revision": "48cc490cc325cbabab5b",
+    "url": "/portfolio/static/js/main.2564c72b.chunk.js"
   },
   {
     "revision": "27e15bd3e25e8b99391d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.7f4d9911a940552d8ea065892d7fca88.js"
+  "/portfolio/precache-manifest.3517cd00c5713c8ba046f1898aebb859.js"
 );
 
 self.addEventListener('message', (event) => {
