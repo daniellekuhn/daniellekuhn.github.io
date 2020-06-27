5fd12c517cef64f2ee314bc903d9c94f86619600
Danielle Kuhn <danielle.r.kuhn@gmail.com>
2020-06-27 17:39:18 -0400

    fixed toggle on navbar to collapse

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,27 +3,24 @@ import styles from './Navbar.module.css'
 import { HashLink as Link } from 'react-router-hash-link'
 
 const Navbar = () => {
-    let aClass = ["nav-link"];
-    aClass.push(styles['link'])
-
     return (
         <div className="fixed-top">
         <nav className={"navbar navbar-expand-lg navbar-dark " +styles['background']}>
         <div className={"container " + styles['overall']}>
             <Link className="navbar-brand" to="/#home">Danielle Kuhn</Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
             </button>
 
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                <li className="nav-item ml-3">
-                    <Link className="nav-link" to="/#AboutMe">About Me</Link>
-                </li>
-                <li className="nav-item ml-3">
-                    <Link className="nav-link" to="/#MyProjects">Software Projects</Link>
-                </li>
-            </ul>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item ml-3">
+                        <Link className="nav-link" to="/#AboutMe">About Me</Link>
+                    </li>
+                    <li className="nav-item ml-3">
+                        <Link className="nav-link" to="/#MyProjects">Software Projects</Link>
+                    </li>
+                </ul>
             </div>
         </div>
         </nav>
