0627bf8d6051545644ed4f8cd429d581f1202308
Danielle Kuhn <danielle.r.kuhn@gmail.com>
2020-07-11 10:02:49 -0500

    updated aboutme details

---
+++ build/precache-manifest.80e33088f913ae018df499200e6acc97.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0c94f15b90f94b504d225ca7e07eb41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5089f9dda41282c15219",
+    "url": "/static/css/main.84dd41fd.chunk.css"
+  },
+  {
+    "revision": "cbde0def514100da4784",
+    "url": "/static/js/2.fd7b11e2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fd7b11e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5089f9dda41282c15219",
+    "url": "/static/js/main.08442108.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "7941ad8d596b72c91803e26c531127d9",
+    "url": "/static/media/Image_mobile.7941ad8d.jpg"
+  },
+  {
+    "revision": "1c2191f81a558e45a58a9f7238034636",
+    "url": "/static/media/arrtozoneTech.1c2191f8.png"
+  },
+  {
+    "revision": "422e1813d365b09ab68e8ecbc6535824",
+    "url": "/static/media/arrtozoneTech.422e1813.webp"
+  },
+  {
+    "revision": "6281db4d0ed84483d74ebefe39d4cdd0",
+    "url": "/static/media/computer.6281db4d.jpg"
+  },
+  {
+    "revision": "72e6e0925562c853fd549bb9a9953430",
+    "url": "/static/media/computer.72e6e092.webp"
+  },
+  {
+    "revision": "07ce29fcc6aa381041247f7ba4d6489d",
+    "url": "/static/media/gigl2.07ce29fc.png"
+  },
+  {
+    "revision": "b51854a1cee5759bd54938ab53f2b43b",
+    "url": "/static/media/gigl2.b51854a1.webp"
+  },
+  {
+    "revision": "4f82363255e071d4cc232c82ad300fb9",
+    "url": "/static/media/giglTech.4f823632.webp"
+  },
+  {
+    "revision": "5ec4cb88cf8b1fc8227107c635478f3c",
+    "url": "/static/media/giglTech.5ec4cb88.png"
+  },
+  {
+    "revision": "c171a4da83e6f501c3755ae04c9c5cd0",
+    "url": "/static/media/image.c171a4da.jpg"
+  },
+  {
+    "revision": "168d6e428cb4486744e82fb60d23849c",
+    "url": "/static/media/output.168d6e42.webp"
+  },
+  {
+    "revision": "b245bb38a182325c0cc7c629a09ebe32",
+    "url": "/static/media/output_main.b245bb38.webp"
+  },
+  {
+    "revision": "1307591bb37e301dc09629e374993169",
+    "url": "/static/media/rsz_arrtozone.1307591b.png"
+  },
+  {
+    "revision": "97815125d1feccc20317a53307e498cc",
+    "url": "/static/media/rsz_arrtozone.97815125.webp"
+  },
+  {
+    "revision": "69b2cb607c1e70ad128847258d8def5a",
+    "url": "/static/media/self.69b2cb60.png"
+  },
+  {
+    "revision": "e9ad17a118d97427a7d417ea3e12acd6",
+    "url": "/static/media/self.e9ad17a1.webp"
+  },
+  {
+    "revision": "081411b0f92525e38c50ecd732129146",
+    "url": "/static/media/trivia.081411b0.png"
+  },
+  {
+    "revision": "544bb9f3829ab0a4aee71426e60c1ab0",
+    "url": "/static/media/trivia.544bb9f3.webp"
+  },
+  {
+    "revision": "f406c9457fcef8e055cf0a1f26d64d4b",
+    "url": "/static/media/triviaTech.f406c945.webp"
+  },
+  {
+    "revision": "f7b042c61ed721509ba6d3e214e77870",
+    "url": "/static/media/triviaTech.f7b042c6.png"
+  },
+  {
+    "revision": "94c69a2117348b1f8cea0649f38b206e",
+    "url": "/static/media/world2.94c69a21.jpeg"
+  },
+  {
+    "revision": "ee049d85618ab89cfaded1829486fccb",
+    "url": "/static/media/world2.ee049d85.webp"
+  }
+]);
